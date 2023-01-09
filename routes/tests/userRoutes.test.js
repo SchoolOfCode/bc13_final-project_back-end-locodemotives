517baa7f5980b5dbfcac941d3a7b461fc792ee10
romanhadjisergis <rhadjisergis@hotmail.com>
2023-01-09 10:45:46 +0000

    user routes supertest

---
+++ routes/tests/userRoutes.test.js
@@ -0,0 +1,31 @@
+import request from "supertest";
+import app from "../../app";
+import { expect, test } from "@jest/globals";
+import { response } from "express";
+
+// import { resetTables } from "../../database/helpers.js";
+// import { pool } from "../../database/index";
+
+// beforeEach(async () => {
+//   await resetTables();
+// });
+
+test("get user by id", async () => {
+  const response = await request(app).get("/user/?id=1");
+  expect(response.status).toEqual(200);
+  expect(response.body).toStrictEqual({
+    success: true,
+    payload: {
+      user_id: 1,
+      email: "roman@hadjisergis.com",
+      password: "password",
+      image_url: expect.any(String),
+      team: "Support Services",
+      name: "Roman Hadjisergis",
+    },
+  });
+});
+
+// afterAll(() => {
+//   pool.end();
+// });
