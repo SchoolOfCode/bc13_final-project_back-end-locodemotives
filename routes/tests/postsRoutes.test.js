d0e9fef6092d7d4d0c5631f44fb282f54b4367c3
Elspeth Brown <elspethrbrown@outlook.com>
2023-01-09 14:36:29 +0000

    finished post routes testing

--- routes/tests/postsRoutes.test.js
+++ routes/tests/postsRoutes.test.js
@@ -128,3 +128,76 @@ describe("Get replies for posts", () => {
     });
   });
 });
+
+describe("Post requests for new post or reply", () => {
+  test("Create new post", async () => {
+    const response = await request(app).post("/posts/post").send({
+      title: "Testing new post",
+      topic: "Testing",
+      body: "How to create a new post?",
+      date_created: "2023-01-09",
+      author: 1,
+    });
+    expect(response.status).toEqual(200);
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: {
+        post_id: 3,
+        title: "Testing new post",
+        topic: "Testing",
+        body: "How to create a new post?",
+        date_created: expect.any(String),
+        author: 1,
+      },
+    });
+  });
+
+  test("Create new reply", async () => {
+    const response = await request(app).post("/posts/reply").send({
+      post: 1,
+      author: 3,
+      body: "This is how you make a reply",
+      date_created: "2023-01-09",
+    });
+    expect(response.status).toEqual(200);
+    expect(response.body).toStrictEqual({
+      success: true,
+      payload: {
+        reply_id: 4,
+        post: 1,
+        author: 3,
+        body: "This is how you make a reply",
+        date_created: expect.any(String),
+      },
+    });
+  });
+});
+
+/* Delete tests - not possible due to foriegn key issues
+describe("Delete request for post or reply", () => {
+  test("Delete a post", async () => {
+    const response = await request(app).delete("/posts/deletePost/1");
+    expect(response.status).toEqual(200);
+    expect(response.body).toStrictEqual({
+      title: "PostgreSQL foreign keys",
+      topic: "PostgreSQL",
+      body: "How to do foreign keys in postgreSQL?",
+      date_created: "2023-01-06",
+      author: 1,
+      post_id: 1,
+    });
+  });
+
+  test("Delete a reply", async () => {
+    const response = await request(app).delete("/posts/deleteReply/1");
+    expect(response.status).toEqual(200);
+    expect(response.body).toStrictEqual({
+      post: 1,
+      author: 2,
+      body: "Use references table_name(table_item)",
+      date_created: "2023-01-07",
+      reply_id: 1,
+    });
+  });
+});
+*/
